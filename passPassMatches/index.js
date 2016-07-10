3ccd5a0235bbc209f02cffcda46b473c8b5a3dca
juleasGoullee <julesgoullee@gmail.com>
2016-07-10 21:08:06 +0200

    docker & logs pass pass

--- passPassMatches/index.js
+++ passPassMatches/index.js
@@ -1,9 +1,13 @@
 require('../config/log');
+const log = require('npmlog');
+const colors = require('colors/safe');
 const path = require('path');
 const consts = require('../config/consts');
 const passPassMatchesFile = require(path.resolve( path.join(consts.DATA_PATH, '/passPassMatches') ));
 
 const passPassMatches = Object.assign([], passPassMatchesFile);
+log.info('step', colors.yellow.bold(`Read from file ${passPassMatches.length} pass pass matches.......\n\n`) );
+
 const uniqueProfiles = require('./uniqueProfiles');
 const inject = require('./inject');
 const newMessage = require('./newMessage');
